cf90657597fb1af92cc3227a3d731ff8a4f89233
DeepAmplitude <118306444+tominik83@users.noreply.github.com>
2024-03-10 09:00:25 +0100

    ?

--- btweb-player.php
+++ btweb-player.php
@@ -4,7 +4,7 @@
  * Plugin Name: Web Radio Player
  * Plugin URI:        https://github.com/tominik83/WordPress-Plugins/tree/d81f3622208d63befa9e14642954c8763e30b3fb/log-reg
  * Description:       Web Player
- * Version:           1.0.0
+ * Version:           1.0
  * Requires at least: 5.2
  * Requires PHP:      7.2 and ...
  * Author:            Mihajlo Tomic
@@ -48,9 +48,13 @@ class BTWEB
 
     public function load_assets()
     {
-        wp_enqueue_style('btweb-player-style', plugin_dir_url(__FILE__) . 'dist/btweb-player.css', array(), 1, 'all');
+        $plugin_path = 'wp-content/plugins/btweb-player/btweb-player.php';
+        $version_data = get_file_data($plugin_path, array('Version'));
+        $version = $version_data[0];
 
-        wp_enqueue_script('btweb-player-script', plugin_dir_url(__FILE__) . 'dist/btweb-player.js', array('jquery'), 1, true);
+        wp_enqueue_style('btweb-player-style', plugin_dir_url(__FILE__) . 'dist/btweb-player.css', array(), $version, 'all');
+
+        wp_enqueue_script('btweb-player-script', plugin_dir_url(__FILE__) . 'dist/btweb-player.js', array('jquery'), $version, true);
     }
 
 
@@ -60,7 +64,40 @@ class BTWEB
     public function load_shortcode()
     { ?>
 
-        Test
+        
+<?php get_header('myradio'); ?>
+
+
+<!-- <div id="ct-widget">
+    <?php dynamic_sidebar('ct-widget'); ?>
+</div> -->
+
+<div id="radio-stations">
+    <div class="radio-station" id="techno">
+        <h2>Techno Chronicle</h2>
+        <audio id="techno-audio" controls>
+            <source src="https://myradio.bibliotehnika.com/listen/techno_chronicle/set.mp3U" type="audio/mpeg">
+        </audio>
+    </div>
+
+    <div class="radio-station" id="prava">
+        <h2>Prava</h2>
+        <audio id="prava-audio" controls>
+            <source src="https://myradio.bibliotehnika.com/listen/prava/DAP1" type="audio/mpeg">
+        </audio>
+    </div>
+
+    <div class="radio-station" id="bibliotehnika">
+        <h2>Bibliotehnika</h2>
+        <audio id="prava-audio" controls>
+            <source src="http://casaos:20080/listen/bibliotehnika/set-2.mp3U" type="audio/mpeg">
+        </audio>
+    </div>
+
+</div>
+
+
+
 
 <?php
     }
